3fd1206bb347e828fdd3036e713c3386c176f9fc
Sahil Madaan <madaan.sahil27@gmail.com>
2025-08-10 12:15:07 +0530

    Commands added

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+
 import speech_recognition as sr
 import win32com.client
+import webbrowser
+import datetime
+import pywhatkit
+import openai
+from Demos.EvtSubscribe_push import query_text
+from pymsgbox import prompt
+from soupsieve.util import lower
+from wikipedia import languages
+
+
+chatStr = ""
 
 def say(text):
     speaker = win32com.client.Dispatch("SAPI.SpVoice")
@@ -8,10 +21,11 @@ def say(text):
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 1
+        # r.pause_threshold = 0.6
         audio = r.listen(source)
         try:
-            query = r.recognize_google(audio)
+            print("Recognizing...")
+            query = r.recognize_google(audio, language="en-in")
             print(f"User said: {query}\n")
             return query
         except Exception as e:
@@ -22,6 +36,45 @@ if __name__ == '__main__':
     say("Hello i am Jarvis AI")
     while True:
         print("Listening...")
-        text = takeCommand()
-        say(text)
+        query = takeCommand()
+        sites = [["youtube", "https://www.youtube.com"], ["wikipedia", "https://www.wikipedia.com"],
+                 ["google", "https://www.google.com"], ]
+        for site in sites:
 
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Opening {site[0]} sir...")
+                webbrowser.open(site[1])
+
+            elif "open music" in query.lower():
+                musicPath = r"D:\Music\Banjaara.mp3"  # use raw string or double backslashes
+                os.startfile(musicPath)
+
+            elif "the time" in query.lower():
+                hour = datetime.datetime.now().strftime("%H")
+                min = datetime.datetime.now().strftime("%M")
+                say(f"Sir, the time is {hour} hours and {minute} minutes")
+
+            elif "open camera" in query.lower():
+                os.system("start microsoft.windows.camera:")
+
+            elif "send whatsapp" in query.lower():
+                now = datetime.datetime.now()
+                pywhatkit.sendwhatmsg_instantly(
+                    "+918082697386",
+                    "Hello from Sahil Madaan!",
+                    tab_close=True,
+                    close_time=2
+                )
+
+            elif "Using artificial Intelligence".lower() in query.lower():
+                ai(prompt=query)
+
+            elif "Jarvis Quit".lower() in query.lower():
+                exit()
+
+            elif "reset chat".lower() in query.lower():
+                chatStr = ""
+
+            else:
+                print("Quitting...")
+                # chat(query)
